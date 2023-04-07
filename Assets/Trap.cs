135ceb2725809a44ea71912c16f1d5b8d942d997
Nathan iMac <nathanpana@gmail.com>
2023-04-07 22:59:43 +0930

    Added ability to place spikes as traps
    Traps can now be added by the player to the map wherever they like.

--- Assets/Trap.cs
+++ Assets/Trap.cs
@@ -7,16 +7,19 @@ public class Trap : MonoBehaviour
 
     public int DPS = 1;
     private bool hasAttacked;
+
+    private bool isTriggerActive = false;
     private float timeSinceLastAttack;
     public float attackCooldown;
     public LayerMask enemyLayer;
 
+    private Collider enemyCollider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("DealDamage", 0, attackCooldown);
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -30,25 +33,58 @@ public class Trap : MonoBehaviour
         }
     }
 
-   
-    private void OnTriggerStay(Collider collision)
+    private void OnTriggerEnter(Collider other)
     {
-        //
+        enemyCollider = other;
+    }
 
-        if (enemyLayer == (enemyLayer | (1 << collision.gameObject.layer)))
-        {
-            collision.gameObject.GetComponent<EnemyBehaviour>().Damage(DPS);
+    void OnTriggerStay(Collider other)
+    {
+        isTriggerActive = true;
+    }
 
-            if (!hasAttacked)
-            {
-                Debug.Log("attacked");
-                hasAttacked = true;
-                timeSinceLastAttack = 0f;
-            }
-        }
-
-     
+    void OnTriggerExit(Collider other)
+    {
+        isTriggerActive = false;
     }
 
 
-}
+    void DealDamage()
+    {
+        if (isTriggerActive)
+        {
+            if (enemyLayer == (enemyLayer | (1 << enemyCollider.gameObject.layer)))
+            {
+                enemyCollider.gameObject.GetComponent<EnemyBehaviour>().Damage(DPS);
+
+                if (!hasAttacked)
+                {
+                    Debug.Log("attacked");
+                    hasAttacked = true;
+                    timeSinceLastAttack = 0f;
+                }
+            }
+
+        }
+
+        /*
+
+        private void OnTriggerEnter(Collider collision)
+        {
+            //
+
+            if (enemyLayer == (enemyLayer | (1 << collision.gameObject.layer)))
+            {
+                collision.gameObject.GetComponent<EnemyBehaviour>().Damage(DPS);
+
+                if (!hasAttacked)
+                {
+                    Debug.Log("attacked");
+                    hasAttacked = true;
+                    timeSinceLastAttack = 0f;
+                }
+            }
+
+        }*/
+    }
+ }
