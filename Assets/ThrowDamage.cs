187dce32a810dd9bdf02d300b49a8ac7950795bb
nati8oy <nathanpana@gmail.com>
2023-08-01 20:48:10 +0930

    Added Throw damage and health bars
    Health bars reducing to show throw damage from player throwing enemies

--- Assets/ThrowDamage.cs
+++ Assets/ThrowDamage.cs
@@ -2,21 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using MoreMountains;
+using MoreMountains.Tools;
 
 public class ThrowDamage : MonoBehaviour
 {
     [SerializeField] private UnityEvent onHit;
     [SerializeField] private float damageThreshold = 10f; // Minimum collision velocity required to trigger damage
+    [SerializeField] private float throwDamage; 
+
+
+    [Header("Health Bar")]
+    [SerializeField] private MMHealthBar healthBar;
+    [SerializeField] private float maxHealth = 100f;
+    [SerializeField] private float currentHealth = 100f;
+    [SerializeField] private float minHealth = 0f;
+
+    // Start is called before the first frame update
+
 
     private Rigidbody rb;
     // Start is called before the first frame update
 
 
-    private void Damage()
+    private void Damage(float damageAmount)
     {
         // Perform damage logic here
 
         onHit.Invoke();
+
+
+        if (currentHealth> minHealth)
+        {
+            currentHealth = currentHealth - damageAmount;
+            healthBar.UpdateBar(currentHealth, minHealth, maxHealth, true);
+        }
+        else
+        {
+            currentHealth = minHealth;
+        }
+
+
     }
 
     // Start is called before the first frame update
@@ -32,7 +58,7 @@ public class ThrowDamage : MonoBehaviour
 
         if (collisionVelocity >= damageThreshold)
         {
-            Damage();
+            Damage(throwDamage);
         }
     }
 
